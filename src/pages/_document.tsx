401d11f98e2d21d78bc4acccd9ddaa63105b6c0e
Fillipe Augusto <fillipeaugustoribeiro@gmail.com>
2022-05-09 16:32:03 -0300

    remove unused variables

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,10 +19,6 @@ export default class MyDocument extends Document {
 				<body>
 					<Main />
 					<NextScript />
-					<script
-						type="text/javascript"
-						src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"
-					/>
 				</body>
 			</Html>
 		);
